2314a464cc254ee8501aabaa6f746ef4149db1e5
JackieSF <jaclanesf@gmail.com>
2022-03-27 11:27:27 -0400

    added changes to choice list function to make sure the game does not accept invalid input, added score count and parameters for winner

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -23,11 +23,49 @@ while True:
     player_choice = input('Choose Your Weapon 1. Rock 2. Paper or 3. Scissors:').lower()
     if player_choice not in choice_list:
         print('Invalid. Please choose again.')
-        player_choice = input('Choose Your Weapon 1. Rock 2. Paper or 3. Scissors:').lower()
     else:
         print('You Have Chosen: ' , player_choice)
-    comp_choice = random.choice(choice_list)
+    
+	comp_choice = random.choice(choice_list)
     print('The Computer has chosen: ' , comp_choice)
-    print(player_choice, 'VS', comp_choice)
+	print(player_choice, 'VS', comp_choice)
+
+	playerScore = 0
+	compScore = 0
+
+	if((player_choice == 'rock' and comp_choice == 'paper') or (player_choice == 'paper' and comp_choice =='scissors')):
+		print('Paper Wins!')
+		winner='paper'
+		if player_choice== winner:
+			playerScore= +1
+		else:
+			compScore= +1
+	if((player_choice == 'paper' and comp_choice == 'scissors') or (player_choice == 'scissors' and comp_choice == 'paper')):
+		print('Scissors win!')
+		winner='scissors'
+		if player_choice== winner:
+			playerScore= +1
+		else:
+			compScore= +1
+	elif ((player_choice == 'rock' and comp_choice == 'scissors') or (player_choice == 'scissors' and comp_choice == 'rock')):
+		print('Rock wins!')
+		winner ='rock'
+		if player_choice== winner:
+			playerScore= +1
+		else:
+			compScore= +1
+
+	while winner in choice_list:
+		if winner == player_choice:
+			print("Player wins")
+	else:
+		print("Computer wins")
+
+	print("Do you want to play again? (Yes or No)")
+	answer = input()
+	if answer == 'no' or answer == 'No':
+		break
+	else:
+		continue 
 
 
